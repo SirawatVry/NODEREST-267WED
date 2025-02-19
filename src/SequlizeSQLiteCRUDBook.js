6ad9f131d9ed472880a52148ee02c2514fe0fd72
SirawatVry <s6606021630061@email.kmutnb.ac.th>
2025-02-19 09:46:55 +0700

    fin

--- src/SequlizeSQLiteCRUDBook.js
+++ src/SequlizeSQLiteCRUDBook.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const sequelize = require('sequelize');
 const Sequelize = require('sequelize');
@@ -50,5 +51,34 @@ app.post('/books',(req,res)=>{
     });
 });
 app.put('books/:id', (req,res)=>{
-    Book.findByPk(req.params.id).then
-})
+    Book.findByPk(req.params.id).then(book =>{
+        if (!book){
+            res.status(404).send('Book not found');
+        }else{
+            book.update(req.body).then(()=>{
+                res.send(book);
+            }).catch(err => {
+                res.status(500).send(err);
+            });
+        }
+    }).catch(err=>{
+        res.status(500).send(err);
+    });
+});
+app.delete('/books/:id',(req,res) =>{
+    Book.findByPk(req.params.id).then(book => {
+        if(!book){
+            res.status(404).send('Book not found');
+        }else{
+            book.destroy().then(() =>{
+                res.send({});
+            }).catch(err => {
+                res.status(500).send(err);
+            });
+        }
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+const port = process.env.PORT || 3000;
+app.listen(port,() => console.log(`Listening on port (http://localhost:${port})`))
